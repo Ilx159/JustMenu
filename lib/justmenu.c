9742f642e4e65b7293ccf9462461f008d9fbdf88
Isaac <113073716+Ilx159@users.noreply.github.com>
2025-04-25 13:29:08 -0300

    justmenu.c criação da função create menu

--- lib/justmenu.c
+++ lib/justmenu.c
@@ -5,24 +5,57 @@
 char*** items;
 char*** menus;
 
-void add_item();
-void add_list();
-void num_del_item(char item[]);
-void name_del_item(unsigned char item);
+void add_item(char *item);
+void add_list(char **item);
+void name_del_item(char *item);
+void num_del_item(unsigned char item);
+void create_menu();
+char * print_menu();
+
+
+
+void create_menu(){
+
+
+    sprintf( ,"+");
+
+    for(unsigned char i = 0; i < width; sprintf(menu,"-"), i++);
+        sprintf(menu,"+\n");
+
+    for(char i = 0; i < array_size; i++){
+        sprintf(menu,"| %u. %-10s|\n", i + 1, items[i]);
+    }
+
+    sprintf(menu,"+");
+
+    for(unsigned char i = 0; i < width; sprintf(menu,"-"), i++);
+
+    sprintf(menu,"+\n");
+
+
+}
+
+
 
 
 char * print_menu(char *items[], unsigned char array_size, unsigned char width){
+
     char * menu;
     width -= 2;
 
     sprintf(menu,"+");
+
     for(unsigned char i = 0; i < width; sprintf(menu,"-"), i++);
-    sprintf(menu,"+\n");
+        sprintf(menu,"+\n");
+
     for(char i = 0; i < array_size; i++){
         sprintf(menu,"| %u. %-10s|\n", i + 1, items[i]);
     }
+
     sprintf(menu,"+");
+
     for(unsigned char i = 0; i < width; sprintf(menu,"-"), i++);
+
     sprintf(menu,"+\n");
 
 
